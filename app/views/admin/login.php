f37cf5adcb95693e33fc151f053f4ea04aafff2d
NguyenNgocDuyPH32135 <duynnph32135@fpt.edu.vn>
2025-07-19 22:30:33 +0700

    test git

--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Đăng nhập quản trị</title>
+    <title>Đăng nhập quản trị </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
